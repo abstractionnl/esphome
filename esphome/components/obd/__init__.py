51b7777e910a2bc2b139561e0079daae9cf09d5b
Marcel Koonstra <marcelkoonstra@embracecloud.nl>
2024-12-23 06:26:33 +0100

    Code cleanup

--- esphome/components/obd/__init__.py
+++ esphome/components/obd/__init__.py
@@ -1,13 +1,12 @@
-import esphome.config_validation as cv
-import esphome.codegen as cg
-import esphome.components.sensor as s
-import esphome.components.binary_sensor as bs
 from esphome import automation
-from esphome.const import (
-    CONF_ID,
-    CONF_INTERVAL,
-    CONF_TRIGGER_ID
-)
+import esphome.codegen as cg
+import esphome.components.binary_sensor as bs
+import esphome.components.sensor as s
+import esphome.config_validation as cv
+from esphome.const import CONF_ID, CONF_INTERVAL, CONF_TIMEOUT, CONF_TRIGGER_ID
+
+CODEOWNERS = ["@abstractionnl"]
+DEPENDENCIES = ["canbus"]
 
 obd_ns = cg.esphome_ns.namespace("obd")
 OBDComponent = obd_ns.class_("OBDComponent", cg.PollingComponent)
@@ -15,48 +14,54 @@ OBDSensor = obd_ns.class_("OBDSensor", s.Sensor, cg.Component)
 OBDBinarySensor = obd_ns.class_("OBDBinarySensor", bs.BinarySensor, cg.Component)
 PIDRequest = obd_ns.class_("PIDRequest", cg.Component)
 OBDPidTrigger = obd_ns.class_(
-    "OBDPidTrigger", 
+    "OBDPidTrigger",
     automation.Trigger.template(cg.std_vector.template(cg.uint8)),
-    cg.Component
+    cg.Component,
 )
 
-CONF_CANBUS_ID = 'canbus_id'
-CONF_ENABLED_BY_DEFAULT = 'enabled_by_default'
-CONF_OBD_ID = 'obd_id'
+CONF_CANBUS_ID = "canbus_id"
+CONF_ENABLED_BY_DEFAULT = "enabled_by_default"
+CONF_OBD_ID = "obd_id"
 CONF_PID_ID = "pid_id"
 CONF_CAN_ID = "can_id"
 CONF_RESPONSE_CAN_ID = "response_can_id"
 CONF_USE_EXTENDED_ID = "use_extended_id"
 CONF_PIDS = "pids"
 CONF_PID = "pid"
-CONF_TIMEOUT = "timeout"
 CONF_REPLY_LENGTH = "reply_length"
 CONF_ON_FRAME = "on_frame"
 CONF_MASK = "mask"
 CONF_SIGNED = "signed"
 
-CONFIG_SCHEMA = cv.Schema({
-  cv.GenerateID(): cv.declare_id(OBDComponent),
-  cv.Required(CONF_CANBUS_ID): cv.use_id("CanbusComponent"),
-  cv.Optional(CONF_ENABLED_BY_DEFAULT, default=False): cv.boolean,
-  cv.Optional(CONF_PIDS): cv.ensure_list(
-      {
-          cv.GenerateID(CONF_ID): cv.declare_id(PIDRequest),
-          cv.Required(CONF_CAN_ID): cv.int_range(min=0, max=0x1FFFFFFF),
-          cv.Required(CONF_PID): cv.int_range(min=0, max=0x1FFFFFFF),
-          cv.Optional(CONF_RESPONSE_CAN_ID): cv.int_range(min=0, max=0x1FFFFFFF),
-          cv.Optional(CONF_USE_EXTENDED_ID, default=False): cv.boolean,
-          cv.Optional(CONF_INTERVAL, default="5s"): cv.positive_time_period_milliseconds,
-          cv.Optional(CONF_TIMEOUT, default="500ms"): cv.positive_time_period_milliseconds,
-          cv.Optional(CONF_REPLY_LENGTH, default=8): cv.positive_int,
-          cv.Optional(CONF_ON_FRAME): automation.validate_automation(
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(OBDComponent),
+        cv.Required(CONF_CANBUS_ID): cv.use_id("CanbusComponent"),
+        cv.Optional(CONF_ENABLED_BY_DEFAULT, default=False): cv.boolean,
+        cv.Optional(CONF_PIDS): cv.ensure_list(
             {
-                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(OBDPidTrigger),
+                cv.GenerateID(CONF_ID): cv.declare_id(PIDRequest),
+                cv.Required(CONF_CAN_ID): cv.int_range(min=0, max=0x1FFFFFFF),
+                cv.Required(CONF_PID): cv.int_range(min=0, max=0x1FFFFFFF),
+                cv.Optional(CONF_RESPONSE_CAN_ID): cv.int_range(min=0, max=0x1FFFFFFF),
+                cv.Optional(CONF_USE_EXTENDED_ID, default=False): cv.boolean,
+                cv.Optional(
+                    CONF_INTERVAL, default="5s"
+                ): cv.positive_time_period_milliseconds,
+                cv.Optional(
+                    CONF_TIMEOUT, default="500ms"
+                ): cv.positive_time_period_milliseconds,
+                cv.Optional(CONF_REPLY_LENGTH, default=8): cv.positive_int,
+                cv.Optional(CONF_ON_FRAME): automation.validate_automation(
+                    {
+                        cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(OBDPidTrigger),
+                    }
+                ),
             }
-          ),
-      }
-  )
-}).extend(cv.COMPONENT_SCHEMA)
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -72,8 +77,8 @@ async def to_code(config):
         pid = pid_conf[CONF_PID]
         use_extended_id = pid_conf[CONF_USE_EXTENDED_ID]
         response_can_id = pid_conf.get(CONF_RESPONSE_CAN_ID)
-        
-        if (response_can_id is None):
+
+        if response_can_id is None:
             response_can_id = can_id | 8
 
         pid_request = cg.new_Pvariable(
@@ -88,6 +93,8 @@ async def to_code(config):
         for trigger_conf in pid_conf.get(CONF_ON_FRAME, []):
             trigger = cg.new_Pvariable(trigger_conf[CONF_TRIGGER_ID], pid_request)
             await cg.register_component(trigger, trigger_conf)
-            await automation.build_automation(trigger, [(cg.std_vector.template(cg.uint8), "data")], trigger_conf)
+            await automation.build_automation(
+                trigger, [(cg.std_vector.template(cg.uint8), "data")], trigger_conf
+            )
 
     return var
--- esphome/components/obd/binary_sensor.py
+++ esphome/components/obd/binary_sensor.py
@@ -1,23 +1,15 @@
-import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import binary_sensor
+import esphome.config_validation as cv
+from esphome.const import CONF_INDEX, CONF_LAMBDA
 from esphome.cpp_generator import LambdaExpression
-from esphome.const import (
-    CONF_LAMBDA,
-    CONF_INDEX,
-)
-from . import (
-    CONF_PID_ID,
-    CONF_MASK,
-    OBDBinarySensor,
-    PIDRequest
-)
+
+from . import CONF_MASK, CONF_PID_ID, OBDBinarySensor, PIDRequest
 
 CONFIG_SCHEMA = cv.All(
     binary_sensor.binary_sensor_schema(
         OBDBinarySensor,
-    )
-    .extend(
+    ).extend(
         {
             cv.Required(CONF_PID_ID): cv.use_id(PIDRequest),
             cv.Optional(CONF_LAMBDA): cv.returning_lambda,
@@ -26,8 +18,9 @@ CONFIG_SCHEMA = cv.All(
         }
     ),
     cv.has_exactly_one_key(CONF_LAMBDA, CONF_INDEX),
-    cv.has_none_or_all_keys(CONF_INDEX, CONF_MASK)
+    cv.has_none_or_all_keys(CONF_INDEX, CONF_MASK),
 )
+
 
 async def to_code(config):
     pid_request = await cg.get_variable(config[CONF_PID_ID])
@@ -35,15 +28,17 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     if lambda_ := config.get(CONF_LAMBDA):
-        template = await cg.process_lambda(lambda_, [(cg.std_vector.template(cg.uint8), "data")], return_type=cg.bool_)
+        template = await cg.process_lambda(
+            lambda_, [(cg.std_vector.template(cg.uint8), "data")], return_type=cg.bool_
+        )
         cg.add(var.set_template(template))
-    
+
     else:
         index = config[CONF_INDEX]
         mask = config[CONF_MASK]
         template = LambdaExpression(
-                f"return (data[{index}] & {mask}) == {mask};",
-                [(cg.std_vector.template(cg.uint8), "data")], 
-                return_type=cg.bool_
-            )
+            f"return (data[{index}] & {mask}) == {mask};",
+            [(cg.std_vector.template(cg.uint8), "data")],
+            return_type=cg.bool_,
+        )
         cg.add(var.set_template(template))
